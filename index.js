9fca07a63d27e6167342268a6c8b460ed57b1824
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-14 13:11:22 -0300

    refactored createInitialUser and begin noticias

--- index.js
+++ index.js
@@ -9,22 +9,26 @@ mongoose.Promise = global.Promise;
 const mongo = process.env.MONGODB || "mongodb://localhost/noticias";
 
 const User = require('./models/user')
+const noticias = require('./routes/noticias')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+app.use('/noticias', noticias)
+
 app.get("/", (req, res) => res.render("index"));
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    createInitialUser()
     app.listen(port, () => console.log("listening"));
   })
   .catch((e) => console.log(e));
 
   const createInitialUser = async() => {
-    const total = await User.count({ username: 'reinaldo'})
+    const total = await User.countDocuments({ username: 'reinaldo'})
     if(total === 0) {
       const user = new User({
         username: 'reinaldo',
