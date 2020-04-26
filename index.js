27adf675eec55e856009c46589c81842381019f7
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-25 23:17:09 -0300

    login e logout com sessions

--- index.js
+++ index.js
@@ -9,47 +9,73 @@ mongoose.Promise = global.Promise;
 
 const mongo = process.env.MONGODB || "mongodb://localhost/noticias";
 
-const User = require('./models/user')
-const noticias = require('./routes/noticias')
-const restrito = require('./routes/restrito')
+const User = require("./models/user");
+const noticias = require("./routes/noticias");
+const restrito = require("./routes/restrito");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({secret: 'fullstack-master'}))
+app.use(session({ secret: "fullstack-master" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 
-
-app.use('/restrito', (req,res,next) => {
-  if('user' in req.session) {
-   return next()
+app.use((req, res, next) => {
+  if ("user" in req.session) {
+    res.locals.user = req.session.user;
   }
-  res.send('precisa logar')
-})
+  next();
+});
 
-app.use('/restrito', restrito)
-app.use('/noticias', noticias)
+app.use("/restrito", (req, res, next) => {
+  if ("user" in req.session) {
+    return next();
+  }
+  res.redirect("/login");
+});
+
+app.use("/restrito", restrito);
+app.use("/noticias", noticias);
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  const isValid = await user.checkPassword(req.body.password);
+
+  if (isValid) {
+    req.session.user = user;
+    res.redirect("/restrito/noticias");
+  } else {
+    res.redirect("/login");
+  }
+  res.send({
+    user,
+    isValid,
+  });
+});
 
 app.get("/", (req, res) => res.render("index"));
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    createInitialUser()
+    createInitialUser();
     app.listen(port, () => console.log("listening"));
   })
   .catch((e) => console.log(e));
 
-  const createInitialUser = async() => {
-    const total = await User.countDocuments({ username: 'reinaldo'})
-    if(total === 0) {
-      const user = new User({
-        username: 'reinaldo',
-        password: 'abc'
-      })
-      await user.save(()=> console.log('opa'))
-    }
-    else {
-      console.log('Usuario cadastrado, vai pro login')
-    }
+const createInitialUser = async () => {
+  const total = await User.countDocuments({ username: "reinaldo" });
+  if (total === 0) {
+    const user = new User({
+      username: "reinaldo",
+      password: "abc",
+    });
+    await user.save(() => console.log("opa"));
+  } else {
+    console.log("Usuario cadastrado, vai pro login");
   }
+};
--- models/user.js
+++ models/user.js
@@ -21,8 +21,20 @@ UserSchema.pre("save", async function (next) {
 
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
-  next()
+  next();
 });
+
+UserSchema.methods.checkPassword = function (password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(isMatch);
+      }
+    });
+  });
+};
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
