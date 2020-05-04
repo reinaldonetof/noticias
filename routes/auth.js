eae02fde719a47242c024402c45ed26697a7e28b
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-03 23:08:15 -0300

    manipular auth pelo role

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const User = require("../models/user");
 router.use((req, res, next) => {
   if ("user" in req.session) {
     res.locals.user = req.session.user;
+    res.locals.role = req.session.role;
   }
   next();
 });
@@ -25,6 +26,7 @@ router.post("/login", async (req, res) => {
 
   if (isValid) {
     req.session.user = user;
+    req.session.role = user.roles[0];
     res.redirect("/restrito/noticias");
   } else {
     res.redirect("/login");
@@ -33,6 +35,15 @@ router.post("/login", async (req, res) => {
     user,
     isValid,
   });
+});
+
+router.get("/change-role/:role", (req, res) => {
+  if ("user" in req.session) {
+    if (req.session.user.roles.indexOf(req.params.role) >= 0) {
+      req.session.role = req.params.role;
+    }
+  }
+  res.redirect("/");
 });
 
 module.exports = router;
