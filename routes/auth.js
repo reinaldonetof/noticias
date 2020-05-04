6268c5163bf296399bba70df54d162f90b3612a0
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-03 23:12:11 -0300

    fixed bugs and npm start: node index.js

--- routes/auth.js
+++ routes/auth.js
@@ -22,19 +22,23 @@ router.get("/logout", (req, res) => {
 
 router.post("/login", async (req, res) => {
   const user = await User.findOne({ username: req.body.username });
-  const isValid = await user.checkPassword(req.body.password);
+  if (user) {
+    const isValid = await user.checkPassword(req.body.password);
 
-  if (isValid) {
-    req.session.user = user;
-    req.session.role = user.roles[0];
-    res.redirect("/restrito/noticias");
+    if (isValid) {
+      req.session.user = user;
+      req.session.role = user.roles[0];
+      res.redirect("/restrito/noticias");
+    } else {
+      res.redirect("/login");
+    }
+    res.send({
+      user,
+      isValid,
+    });
   } else {
     res.redirect("/login");
   }
-  res.send({
-    user,
-    isValid,
-  });
 });
 
 router.get("/change-role/:role", (req, res) => {
