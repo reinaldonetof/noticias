6ccc1904c5e176313a3988374d9d6ac95d160cdc
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-07 23:28:05 -0300

    novo jeito de faze o login

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,41 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    const user = await User.findOne({ username });
+    if (user) {
+      const isValid = await user.checkPassword(password);
+      if (isValid) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } else {
+      return done, false;
+    }
+  })
+);
+
 router.use((req, res, next) => {
-  if ("user" in req.session) {
-    res.locals.user = req.session.user;
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+    if (!req.session.role) {
+      req.session.role = req.user.roles[0];
+    }
     res.locals.role = req.session.role;
   }
   next();
@@ -20,26 +52,14 @@ router.get("/logout", (req, res) => {
   });
 });
 
-router.post("/login", async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  if (user) {
-    const isValid = await user.checkPassword(req.body.password);
-
-    if (isValid) {
-      req.session.user = user;
-      req.session.role = user.roles[0];
-      res.redirect("/restrito/noticias");
-    } else {
-      res.redirect("/login");
-    }
-    res.send({
-      user,
-      isValid,
-    });
-  } else {
-    res.redirect("/login");
-  }
-});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: false,
+  })
+);
 
 router.get("/change-role/:role", (req, res) => {
   if ("user" in req.session) {
